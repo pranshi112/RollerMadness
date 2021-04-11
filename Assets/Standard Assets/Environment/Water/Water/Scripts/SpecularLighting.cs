7822273a5195b3d9e84b3f6fc76d66fb02413de6
pranshi-jindal <apranshi11@gmail.com>
2021-04-12 00:11:32 +0530

    Remove extra files

--- Assets/Standard Assets/Environment/Water/Water/Scripts/SpecularLighting.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8efe4351abfb1e260fea4fbd0bc389a02bef18085e390a98d767a04c70267b2b
-size 832
--- Assets/Standard Assets/ParticleSystems/Scripts/SmokeParticles.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:972d23328f702f2e287d74bdccea0dd6cc2eca0473929f8d4679652874032b79
-size 440
